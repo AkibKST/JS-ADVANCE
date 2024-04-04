9f114f51224b9c689a7d1a1cc64ff834d7c05f23
AkibKST <akibkst22@gmail.com>
2024-04-04 10:08:45 +0600

    learn export, export default, template string

---
+++ index.js
@@ -0,0 +1,6 @@
+import greetDev, {greetNewLevelDevelopers, greetSuperDevelopers,greetUltraDevelopers} from "./template_srting.js";
+
+greetDev("Akibul Islam");
+greetNewLevelDevelopers("Anamika Khanom");
+greetUltraDevelopers("jhongkar");
+greetSuperDevelopers("mir");
---
+++ template_srting.js
@@ -0,0 +1,26 @@
+const greetNextLevelDevelopers = (name) =>{
+    const greetMsg = "Hello " + name + ", welcome to the next level journey!";
+    const newGreetMsg = `Hello ${name} welcome to the next level journey!`;
+    console.log(newGreetMsg);
+};
+
+export const greetNewLevelDevelopers = (name) =>{
+    const greetMsg = "Hello " + name + ", welcome to the next level journey!";
+    const newGreetMsg = `Hello ${name} welcome to the next level journey!`;
+    console.log(newGreetMsg);
+};
+
+const greetSuperDevelopers = (name) =>{
+    const greetMsg = "Hello " + name + ", welcome to the next level journey!";
+    const newGreetMsg = `Hello Super ${name} welcome to the next level journey!`;
+    console.log(newGreetMsg);
+};
+
+const greetUltraDevelopers = (name) =>{
+    const greetMsg = "Hello " + name + ", welcome to the next level journey!";
+    const newGreetMsg = `Hello Ultra ${name} welcome to the next level journey!`;
+    console.log(newGreetMsg);
+};
+
+export {greetSuperDevelopers, greetUltraDevelopers}
+ export default greetNextLevelDevelopers;
